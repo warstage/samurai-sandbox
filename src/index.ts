ec16dd7336e7f2a13e5ffbc73d07d1da2f90579b
Felix Ungman <felix@ungman.com>
2021-02-19 08:56:44 +0100

    Fix spurious request to CreateMatch

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,18 @@ if (!RuntimeConfiguration.tryAutoRedirect()) {
     runtime.startup(RuntimeConfiguration.autoDetect());
     const navigator = new Navigator(runtime);
     const search = new URLSearchParams(window.location.search);
-    const scenario = new Scenario(navigator, search.get('mode'));
+    let scenario = new Scenario(navigator, search.get('mode'));
     navigator.lobby.onEnterLobby.subscribe(async () => {
-        const match = await navigator.createMatch(scenario.getParams());
-        scenario.startup(match);
+        if (scenario) {
+            const match = await navigator.createMatch(scenario.getParams());
+            scenario.startup(match);
+        }
     });
     navigator.battle.onEnterBattle.subscribe(() => {
     });
     navigator.lobby.onLeaveMatch.subscribe(() => {
         scenario.shutdown();
+        scenario = null;
         navigator.system.federation.requestService('EnterLobby', {
             lobbyId: '000000000000000000000001' // samuraiwars.net online lobby-id
         }).then(() => {}, err => {
